a4f6b0d8208cfc9731dd4128509dd8f0b4997dcf
stevebatcup <steve.batcup@gmail.com>
2023-02-20 12:31:47 +0000

    Show error if xml not uploaded

--- app/controllers/food_products_controller.rb
+++ app/controllers/food_products_controller.rb
@@ -1,5 +1,22 @@
 class FoodProductsController < ApplicationController
-  def new
+  before_action :set_default_food_product
+
+  def new; end
+
+  def create
+    unless params[:file].present? 
+      flash[:error] = "Please make sure to upload an XML file"
+      return redirect_to new_food_product_path
+    end
+    
+    foo
+
+  end
+
+  private 
+
+  def set_default_food_product
     @food_product = FoodProduct.new
   end
+
 end
--- spec/system/import_food_products_spec.rb
+++ spec/system/import_food_products_spec.rb
@@ -1,13 +1,21 @@
 require "rails_helper"
 
 RSpec.describe "Import Food Products", type: :system, js: true do
-  scenario "shows the import XML form" do
+  before :each do
     visit new_food_product_url
+  end
+
+  scenario "shows the import XML form" do
     expect(page).to have_content "Upload a food product data file"
     expect(page).to have_selector("input[value='Upload']")
   end
 
   xscenario "submits the import XML form with valid data and returns a success message" do
+  end
+
+  scenario "submits the import XML form without uploading a file and returns an error message" do
+    page.find("input[value='Upload']").click
+    expect(page).to have_content "Please make sure to upload an XML file"
   end
 
   xscenario "submits the import XML form with invalid data and returns an error message" do
