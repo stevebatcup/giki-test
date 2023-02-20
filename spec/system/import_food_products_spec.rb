13f84547c7609295c21e949047efffd519deb27e
stevebatcup <steve.batcup@gmail.com>
2023-02-20 10:20:54 +0000

    Integration test blueprint for importing food products

---
+++ spec/system/import_food_products_spec.rb
@@ -0,0 +1,12 @@
+require "rails_helper"
+
+RSpec.describe "Import Food Products", type: :system, js: true do
+  scenario "shows the import XML form" do
+  end
+
+  scenario "submits the import XML form with valid data and returns a success message" do
+  end
+
+  scenario "submits the import XML form with invalid data and returns an error message" do
+  end
+end
